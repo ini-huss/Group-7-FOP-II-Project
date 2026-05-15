ffc136deb03eb1f8a0a19363026d508327b7008a
hanniya5003 <hananfedlu33@gmail.com>
2026-05-15 16:23:39 +0400

    Hanan-0696/17

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE CAFETERIA;
+USE cafeteria;
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    user_phone VARCHAR(15) UNIQUE NOT NULL
+);
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_phone VARCHAR(15),
+    items VARCHAR(100), 
+    type VARCHAR(20),   -- Dine-in or Takeaway
+    status VARCHAR(20) DEFAULT 'Pending',
+    -- Link to users table via user_phone
+    FOREIGN KEY (user_phone) REFERENCES users(user_phone) 
+);
+CREATE TABLE menu_items (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    category VARCHAR(20)
+);
+CREATE TABLE feedback (
+    feedback_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_phone VARCHAR(15),
+    rating INT CHECK (rating BETWEEN 1 AND 5), 
+    comment TEXT,
+    -- Link to users table via user_phone
+    FOREIGN KEY (user_phone) REFERENCES users(user_phone)
+);
+
