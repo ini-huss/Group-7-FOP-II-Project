26365216c59b36e9f497a30d503decc43da5adfa
ini-huss <inebo4632@gmail.com>
2026-05-15 16:17:24 +0400

    Final src

---
+++ src/queries.sql
@@ -0,0 +1,12 @@
+INSERT INTO menu_items (item_id,name, price, category) VALUES 
+('1','Beyaynetu', 120.00, 'Lunch'),
+('2','Shiro', 80.00, 'Lunch'),
+('3','Firfir', 90.00, 'Breakfast'),
+('4','Coffee', 25.00, 'Drink'),
+('5','Tea', 15.00, 'Drink');
+INSERT INTO menu_items (item_id,name, price, category) VALUES 
+('6','Kitfo', 125.00, 'Lunch'),
+('7','Rice', 205.00, 'Breakfast'),
+('8','Pasta', 105.00, 'Breakfast'),
+('9','Soya', 220.00, 'Lunch'),
+('10','Chechebsa', 300.00, 'Special');
